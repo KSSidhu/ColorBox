cf822314fde47cafa002bb6c5e281f246a53f922
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2019-08-07 21:10:06 -0700

    Add Select and Material UI

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -7,7 +7,8 @@ class Palette extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			level: 500
+			level: 500,
+			format: 'hex'
 		};
 	}
 
@@ -15,13 +16,17 @@ class Palette extends Component {
 		this.setState({ level });
 	};
 
+	changeFormat = (val) => {
+		this.setState({ format: val });
+	};
+
 	render() {
 		const { colors } = this.props.palette;
-		const { level } = this.state;
-		const colorBoxes = colors[level].map((color) => <ColorBox background={color.hex} name={color.name} />);
+		const { level, format } = this.state;
+		const colorBoxes = colors[level].map((color) => <ColorBox background={color[format]} name={color.name} />);
 		return (
 			<div className="Palette">
-				<Navbar level={level} changeLevel={this.changeLevel} />
+				<Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
 				<div className="Palette-colors">{colorBoxes}</div>
 				{/* footer eventually */}
 			</div>
