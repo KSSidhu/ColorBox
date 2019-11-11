e17bd520fffa2a579162da680173ee105d5c2c31
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2019-11-10 16:45:20 -0800

    Add delete to DraggableColorBox

--- colors-app/src/NewPaletteForm.js
+++ colors-app/src/NewPaletteForm.js
@@ -136,6 +136,12 @@ class NewPaletteForm extends Component {
 		});
 	};
 
+	removeColor = (colorName) => {
+		this.setState({
+			colors: this.state.colors.filter((color) => color.name !== colorName)
+		});
+	};
+
 	render() {
 		const { classes, theme } = this.props;
 		const { open } = this.state;
@@ -231,7 +237,14 @@ class NewPaletteForm extends Component {
 					})}
 				>
 					<div className={classes.drawerHeader} />
-					{this.state.colors.map((color) => <DraggableColorBox color={color.color} name={color.name} />)}
+					{this.state.colors.map((color) => (
+						<DraggableColorBox
+							key={color.name}
+							color={color.color}
+							name={color.name}
+							handleClick={() => this.removeColor(color.name)}
+						/>
+					))}
 				</main>
 			</div>
 		);
