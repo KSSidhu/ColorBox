a4569f13ef8f02b9bd4d34ae7df66ca299c33f62
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2020-01-04 16:38:29 -0800

    Move styles out and add constants file

--- colors-app/src/NewPaletteForm.js
+++ colors-app/src/NewPaletteForm.js
@@ -10,66 +10,8 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import DraggableColorList from './DraggableColorList';
 import { Button } from '@material-ui/core';
 import { arrayMove } from 'react-sortable-hoc';
+import styles from './styles/NewPaletteFormStyles';
 import clsx from 'clsx';
-
-const drawerWidth = 400;
-const appBarHeight = 64;
-
-const styles = (theme) => ({
-	root: {
-		display: 'flex'
-	},
-	hide: {
-		display: 'none'
-	},
-	drawer: {
-		width: drawerWidth,
-		flexShrink: 0
-	},
-	drawerPaper: {
-		width: drawerWidth,
-		display: 'flex',
-		alignItems: 'center'
-	},
-	drawerHeader: {
-		display: 'flex',
-		alignItems: 'center',
-		padding: theme.spacing(0, 1),
-		...theme.mixins.toolbar,
-		justifyContent: 'flex-end'
-	},
-	content: {
-		flexGrow: 1,
-		height: `calc(100vh - ${appBarHeight}px)`,
-		padding: theme.spacing(3),
-		transition: theme.transitions.create('margin', {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen
-		}),
-		marginLeft: -drawerWidth
-	},
-	contentShift: {
-		transition: theme.transitions.create('margin', {
-			easing: theme.transitions.easing.easeOut,
-			duration: theme.transitions.duration.enteringScreen
-		}),
-		marginLeft: 0
-	},
-	container: {
-		width: '90%',
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'center',
-		alignItems: 'center',
-		height: '100%'
-	},
-	buttons: {
-		width: '100%'
-	},
-	button: {
-		width: '50%'
-	}
-});
 
 class NewPaletteForm extends Component {
 	static defaultProps = {
